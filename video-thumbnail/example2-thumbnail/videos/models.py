6deb201aaba4d1c9db1a541ddbcef26e950efb29
mittya <mittya@foxmail.com>
2018-01-24 09:52:39 +0800

    video name to pinyin

--- video-thumbnail/example2-thumbnail/videos/models.py
+++ video-thumbnail/example2-thumbnail/videos/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
+import pypinyin
+
 from .storage import OverwriteStorage
+
+
+def filename_handler(isinstance, filename):
+    """
+    视频有中文时转成拼音再上传
+    """
+    return 'video/%s' % pypinyin.slug(filename, separator='')
 
 
 class Video(models.Model):
     title = models.CharField(max_length=200)
-    video = models.FileField(upload_to='video', storage=OverwriteStorage())
+    video = models.FileField(upload_to=filename_handler, storage=OverwriteStorage())
     video_thumb = models.ImageField(null=True, blank=True, upload_to='video/thumbnail')
     created_at = models.DateTimeField(auto_now_add=True)
 
